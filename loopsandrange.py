70af812323d0deec0c4e55405c57b0b7715f9f74
AmitMali <amit23289@gmail.com>
2023-10-04 19:29:39 +0530

    intro to range and loops

---
+++ loopsandrange.py
@@ -0,0 +1,23 @@
+# print(list(range(10)))
+# print(list(range(5,20)))
+# print(list(range(5,20,4)))
+
+# no=1
+# while(no<=10):
+#     print(no)
+#     no=no+1
+
+# def printNoTable(tableOf):
+#     no=1
+#     while(no<=10):
+#         print(no*tableOf)
+#         no=no+1
+
+# printNoTable(2)
+# printNoTable(17)
+
+# for n in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]:
+#     print(n)
+
+for n in range(1,11):
+    print(n*2)
